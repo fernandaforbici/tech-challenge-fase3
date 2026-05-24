cf51c0f8a090429ad81f9dd3e798f85799da1b9a
prof-fernandaforbici <fernandaforbici.aulas@gmail.com>
2026-05-23 21:39:43 -0300

    Autenticação

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,60 @@
+import { useState, type FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
 export function Login() {
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const validEmail = "admin@blog.com";
+        const validPassword = "123456";
+
+        try {
+            if (email === validEmail && password === validPassword) {
+                //await login(email, password);
+                localStorage.setItem("@blog:token", "token-fake-123");
+                navigate("/admin");
+            } else {
+                alert("Credenciais inválidas. Por favor, tente novamente.");
+            }
+        } catch (err) {
+            setError("Credenciais inválidas. Por favor, tente novamente.");
+        }
+    }
+
     return (
-        <div>
+        <section>
             <h1>Login do Professor</h1>
-        </div>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Entrar</button>
+            </form>
+        </section>
     );
 }
